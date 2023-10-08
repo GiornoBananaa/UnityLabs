a573cef43baefee1f265312ee7d51979fdee2c52
Kirillov_Alexei <alexeikirillovigorevich@mail.ru>
2023-10-08 16:14:53 +0300

    Finished task

--- Assets/_Source/UISystem/ResourceAddingPanel.cs
+++ Assets/_Source/UISystem/ResourceAddingPanel.cs
@@ -12,18 +12,33 @@ namespace UISystem
         [SerializeField] private TMP_InputField _inputField;
         [SerializeField] private TMP_Dropdown _dropdown;
         [SerializeField] private Button _addButton;
+        [SerializeField] private TMP_Text _gunpowderQuantityText;
+        [SerializeField] private TMP_Text _oilQuantityText;
+        [SerializeField] private TMP_Text _uraniumQuantityText;
 
         private void Awake()
         {
             _addButton.onClick.AddListener(AddButton);
+            _dropdown.options.Clear();
+            _dropdown.options.Add(new TMP_Dropdown.OptionData("Gunpowder"));
+            _dropdown.options.Add(new TMP_Dropdown.OptionData("Oil"));
+            _dropdown.options.Add(new TMP_Dropdown.OptionData("Uranium"));
+            UpdateQuantity();
         }
 
         private void AddButton()
         {
             int quantity = 0;
             int.TryParse(_inputField.text, out quantity);
-            
             ResourceBank.Instance.AddResource((ResourceType)_dropdown.value, quantity);
+            UpdateQuantity();
+        }
+
+        private void UpdateQuantity()
+        {
+            _gunpowderQuantityText.text = ResourceBank.Instance.GetQuantity(ResourceType.Gunpowder).ToString();
+            _oilQuantityText.text = ResourceBank.Instance.GetQuantity(ResourceType.Oil).ToString();
+            _uraniumQuantityText.text = ResourceBank.Instance.GetQuantity(ResourceType.Uranium).ToString();
         }
     }
 }
